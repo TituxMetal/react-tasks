8b9fe2f727d57d6ffb75c4881f1e429dd68c6e7c
Titux Metal <github@lgdweb.fr>
2021-04-25 17:12:12 +0200

    Updating TaskForm.js file in NewTask; using the TasksContext for adding task to firebase

--- src/components/NewTask/TaskForm.js
+++ src/components/NewTask/TaskForm.js
@@ -1,17 +1,30 @@
-import React, { useRef } from 'react'
+import React, { useContext, useRef, useState } from 'react'
+
+import { TasksContext } from '~/context'
 
 import './TaskForm.css'
 
 const TaskForm = () => {
+  const [isSending, setIsSending] = useState(false)
   const textInputRef = useRef()
+  const { addTask, error } = useContext(TasksContext)
 
-  const onSubmitHandler = event => event.preventDefault()
+  const onSubmitHandler = event => {
+    event.preventDefault()
+    setIsSending(true)
+    addTask(textInputRef.current.value)
+    textInputRef.current.value = ''
+    setIsSending(false)
+  }
 
   return (
-    <form className='form' onSubmit={onSubmitHandler}>
-      <input type='text' ref={textInputRef} />
-      <button type='submit'>Add Task</button>
-    </form>
+    <>
+      <form className='form' onSubmit={onSubmitHandler}>
+        <input type='text' ref={textInputRef} />
+        <button type='submit'>{isSending ? 'Sending...' : 'Add Task'}</button>
+      </form>
+      {error && <p>{error}</p>}
+    </>
   )
 }
 
