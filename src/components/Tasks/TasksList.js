8725ae065aad05f9e8734956e02a594e15269fa8
Titux Metal <github@lgdweb.fr>
2021-04-25 17:08:51 +0200

    Updating TasksList.js file in Tasks; using the TasksContext to showing tasks coming from firebase

--- src/components/Tasks/TasksList.js
+++ src/components/Tasks/TasksList.js
@@ -1,24 +1,32 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import { Section } from '~/components'
+import { TasksContext } from '~/context'
 
 import TaskItem from './TaskItem'
 import './TaskList.css'
 
-const TasksList = ({ tasks }) => (
-  <Section>
-    <div className='container'>
-      {tasks.length > 0 ? (
-        <ul>
-          {tasks.map(task => (
-            <TaskItem key={task.id}>{task.text}</TaskItem>
-          ))}
-        </ul>
-      ) : (
-        <h2>No tasks found. Start adding some!</h2>
-      )}
-    </div>
-  </Section>
-)
+const TasksList = () => {
+  const { isLoading, error, tasks } = useContext(TasksContext)
+
+  return (
+    <Section>
+      <div className='container'>
+        {error && <p>{error}</p>}
+        {!isLoading && tasks.length > 0 && (
+          <ul>
+            {tasks.map(task => (
+              <TaskItem key={task.id}>{task.text}</TaskItem>
+            ))}
+          </ul>
+        )}
+        {!isLoading && tasks.length === 0 && (
+          <p>No task found, start adding some!</p>
+        )}
+        {isLoading && <p>Loading...</p>}
+      </div>
+    </Section>
+  )
+}
 
 export default TasksList
