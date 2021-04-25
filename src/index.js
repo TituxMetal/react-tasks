3f13fea8dfde979f6528fbfe918c1ea33c1cb7da
Titux Metal <github@lgdweb.fr>
2021-04-25 17:01:43 +0200

    Updating index.js and index.html files; fix compiling and page refresh slowdown by adding fonts and css imports in index.js file and removing css link from index.html file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '@fontsource/noto-sans-jp/400.css'
+import '@fontsource/noto-sans-jp/700.css'
+
+import './index.css'
 
 import App from './App'
 
