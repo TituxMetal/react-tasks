361a0c0260ab1e172191c93f4864ee3d8cb3281a
Titux Metal (DEV) <github@lgdweb.fr>
2021-10-28 09:56:49 +0200

    Updating Home page component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import tw, { styled } from 'twin.macro'
 
-import { TaskForm, Tasks } from '~/components'
+import { TaskForm, TaskList } from '~/components'
 import { TasksProvider } from '~/context'
 
 const Section = styled.section(tw`my-4 mx-auto p-8 rounded-xl w-10/12 bg-gray-700 max-w-screen-lg`)
@@ -10,7 +10,7 @@ const Home = () => (
   <TasksProvider>
     <Section>
       <TaskForm />
-      <Tasks />
+      <TaskList />
     </Section>
   </TasksProvider>
 )
